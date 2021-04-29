33b540634e995988ff6209d3167420696267d991
Alisson <alisson.corrales24@gmail.com>
2021-04-29 10:59:04 -0700

    managed to get numbers to turn into binary!!  4/29/2021

--- addingBinary.js
+++ addingBinary.js
@@ -5,7 +5,7 @@
 let sum = "";
 //these two are going to be turned int binary numbers
 //let termOne = 9; // should be 1001 in binary
-let termOne = 12; // should be 1100 in binary
+let termOne = 3; // should be 1100 in binary
 let termTwo = 10; //should be 1010 in binary
 
 //two loops to run the numbers and return them as the binary numbers
@@ -19,7 +19,6 @@ let fourth = 0;
 let fifth = 0;
 
 //makes termOne a binary #
-let turnBinary = () => {}
 for(let i = 0; i < 1; i++ ){
     let remainderOf8 = 0;
     let remainderOf4 = 0;
@@ -27,36 +26,57 @@ for(let i = 0; i < 1; i++ ){
     let remainderOf1 = 0;
 
     //
-    if(termOne <= 8){
-        remainderOf8 = Math.ceil(8/termOne);
-    }else if(termOne > 8){remainderOf8 = 0}
+    if(termOne < 8){
+        remainderOf8 = 0;
+    }else if(termOne >= 8)
+    {remainderOf8 = 1}
+    if(termOne < 8){
+        termOne = termOne
+    }else{termOne = termOne - 8}
     //
-    /*if(remainderOf8 <= 4){
-        remaindfirsterOf4 = Math.ceil(4);
-    }else if(remainderOf8 > 4){remainderOf4 = 0}*/
+    if(termOne < 4){
+        remainderOf4 = 0;
+    }else if(termOne >= 4)
+    {remainderOf4 = 1}
+    if(termOne < 4){
+        termOne = termOne
+    }else{termOne = termOne - 4}
+    //
+    if(termOne < 2){
+        remainderOf2 = 0;
+    }else if(termOne >= 2)
+    {remainderOf2 = 1}
+    if(termOne < 2){
+        termOne = termOne
+    }else{termOne = termOne - 2}
+    //
+    if(termOne < 1){
+        remainderOf1 = 0;
+    }else if(termOne >= 1)
+    {remainderOf1 = 1}
 
-    console.log(remainderOf8, remainderOf4, remainderOf2, remainderOf1)
+
 
     //turns the first number into a 1 or a 0
-    if(remainderOf8 <= 1 ){
+    if(remainderOf8 >= 1 ){
         first = 1;
     }else if(remainderOf8 < 1 || remainderOf8 == 0){
         first = 0
     }
     //turns the second number into a 1 or a 0
-    if(remainderOf4 == 1){
+    if(remainderOf4 >= 1){
         second = 1;
     }else if(remainderOf4 < 1 ||remainderOf4 == 0){
         second = 0
     }
     //turns the third number into a 1 or a 0
-    if(remainderOf2 == 1){
+    if(remainderOf2 >= 1){
         third = 1;
     }else if(remainderOf2 < 1 || remainderOf2 == 0){
         third = 0
     }
     //turns the fourth number into a 1 or a 0
-    if(remainderOf1 == 1){
+    if(remainderOf1 >= 1){
         fourth = 1;
     }else if(remainderOf1 < 1 || remainderOf1 == 0){
         fourth = 0
@@ -64,6 +84,7 @@ for(let i = 0; i < 1; i++ ){
 
     console.log(`${first} ${second} ${third} ${fourth}`)
 }
+
 
 //makes termTwo a binary #
 
